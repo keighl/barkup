201cf9346f1d1822f45b75d3ac78f15a7ec9f360
Armando Couto <coutoarmando@gmail.com>
2020-02-17 15:16:12 -0300

    Adiciona parametro de senha

--- postgres.go
+++ postgres.go
@@ -29,7 +29,7 @@ type Postgres struct {
 func (x Postgres) Export() *ExportResult {
 	result := &ExportResult{MIME: "application/x-tar"}
 	result.Path = fmt.Sprintf(`bu_%v_%v.sql.tar.gz`, x.DB, time.Now().Unix())
-	options := append(x.dumpOptions(), "-Fc", fmt.Sprintf(`-f%v`, result.Path))
+	options := append(x.dumpOptions(), " -F c ", fmt.Sprintf(` -f %v `, result.Path))
 	//out, err := exec.Command(x.PGDumpCmd, options...).Output()
 	out, err := exec.Command("bash", "-c", fmt.Sprint("echo '", x.Password, "' | ", x.PGDumpCmd, options[0], options[1], options[2], options[3], options[4], options[5], options[6])).Output()
 	if err != nil {
@@ -42,19 +42,19 @@ func (x Postgres) dumpOptions() []string {
 	options := x.Options
 
 	if x.DB != "" {
-		options = append(options, fmt.Sprintf(`-d%v`, x.DB))
+		options = append(options, fmt.Sprintf(` -d %v `, x.DB))
 	}
 
 	if x.Host != "" {
-		options = append(options, fmt.Sprintf(`-h%v`, x.Host))
+		options = append(options, fmt.Sprintf(` -h %v `, x.Host))
 	}
 
 	if x.Port != "" {
-		options = append(options, fmt.Sprintf(`-p%v`, x.Port))
+		options = append(options, fmt.Sprintf(` -p %v `, x.Port))
 	}
 
 	if x.Username != "" {
-		options = append(options, fmt.Sprintf(`-U%v`, x.Username))
+		options = append(options, fmt.Sprintf(` -U %v `, x.Username))
 	}
 
 	return options
