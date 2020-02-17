2c427623b0703eb3e2ff888c4e482e7e56d4b6ee
Armando Couto <coutoarmando@gmail.com>
2020-02-17 15:13:39 -0300

    Adiciona parametro de senha

--- postgres.go
+++ postgres.go
@@ -30,7 +30,8 @@ func (x Postgres) Export() *ExportResult {
 	result := &ExportResult{MIME: "application/x-tar"}
 	result.Path = fmt.Sprintf(`bu_%v_%v.sql.tar.gz`, x.DB, time.Now().Unix())
 	options := append(x.dumpOptions(), "-Fc", fmt.Sprintf(`-f%v`, result.Path))
-	out, err := exec.Command(x.PGDumpCmd, options...).Output()
+	//out, err := exec.Command(x.PGDumpCmd, options...).Output()
+	out, err := exec.Command("bash", "-c", fmt.Sprint("echo '", x.Password, "' | ", x.PGDumpCmd, options[0], options[1], options[2], options[3], options[4], options[5], options[6])).Output()
 	if err != nil {
 		result.Error = makeErr(err, string(out))
 	}
@@ -39,8 +40,6 @@ func (x Postgres) Export() *ExportResult {
 
 func (x Postgres) dumpOptions() []string {
 	options := x.Options
-
-	options = append(options, fmt.Sprint("echo '", x.Password, "' | "))
 
 	if x.DB != "" {
 		options = append(options, fmt.Sprintf(`-d%v`, x.DB))
